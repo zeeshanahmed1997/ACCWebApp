9c466c4734300829da7c7c364acb2836af5f92cc
Zeeshan <mj20abm@herts.ac.uk>
2023-06-19 22:10:34 +0100

    Added views

---
+++ ACCWebApp/wwwroot/js/Navigation.js
@@ -0,0 +1,11 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var navigationSwitch = document.querySelector('.navigation-switch');
+    var navigationBar = document.querySelector('.navigation');
+    var contentContainer = document.querySelector('.content-container');
+
+    navigationSwitch.addEventListener('change', function () {
+        navigationBar.classList.toggle('hidden');
+
+        contentContainer.classList.toggle('hidden');
+    });
+});
--- Domain/Models/SeasonType.cs
+++ Domain/Models/SeasonType.cs
@@ -1,16 +1,14 @@
-﻿//using System;
-//namespace Domain.Models
-//{
-//    // Season Model
-//    public class SeasonType
-//    {
-//        public int SeasonId { get; set; }
-//        public string SeasonName { get; set; }
+﻿using System;
+namespace Domain.Models
+{
+    // Season Model
+    public class SeasonType
+    {
+        public int SeasonId { get; set; }
+        public string SeasonName { get; set; }
 
-//        public ICollection<Clothing> Clothings { get; set; }
-//    }
+        public ICollection<Clothing> Clothings { get; set; }
+    }
 
-
-
-//}
+}
 
