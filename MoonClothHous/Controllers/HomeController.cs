52f9b2c36dbdfdee45b88a63401d9fa62d959494
Zeeshan <mj20abm@herts.ac.uk>
2023-08-03 18:52:37 +0100

    Started Moon CLoth House UI

---
+++ MoonClothHous/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MoonClothHous.Models;
+using System.IO;
+
+namespace MoonClothHous.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Upload()
+        {
+            var file = Request.Form.Files["videoFile"]; // Access the uploaded file using the form field name
+
+            if (file != null && file.Length > 0)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "uploads", fileName);
+
+                try
+                {
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                    return Json(new { message = "File uploaded successfully!" });
+                }
+                catch
+                {
+                    return Json(new { message = "Error uploading the file." });
+                }
+            }
+            else
+            {
+                return Json(new { message = "Please select a file to upload." });
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
