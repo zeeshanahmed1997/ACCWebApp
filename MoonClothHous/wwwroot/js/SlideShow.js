79e284e1288b00574ece7de6a2307c87728f0e6a
Waleed Ali <waleed.ali@rootpointers.com>
2023-08-07 17:50:44 +0500

    Peoduct Landing page in progress

---
+++ MoonClothHous/wwwroot/js/SlideShow.js
@@ -0,0 +1,14 @@
+﻿const slider = document.querySelector(".slider");
+let slideIndex = 0;
+
+function showSlide(index) {
+    slider.style.transform = `translateX(-${index * 100}%)`;
+}
+
+function autoSlide() {
+    slideIndex = (slideIndex + 1) % 3; // Change '3' to the number of slides
+    showSlide(slideIndex);
+}
+
+// Start auto-sliding every 3 seconds
+setInterval(autoSlide, 3000);
